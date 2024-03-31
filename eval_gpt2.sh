f26d56f5e4b92f25711d7ba825ebc36e8b75e4bd
zhqiu <38790914+zhqiu@users.noreply.github.com>
2024-03-31 22:16:34 +0800

    Delete eval_gpt2.sh

--- eval_gpt2.sh
+++
@@ -1,7 +0,0 @@
-python ./deepy.py evaluate.py configs/125M.yml owt2_setup.yml --eval_results_prefix tau1.0 \
-	--eval_tasks logiqa mathqa hellaswag swag lambada_openai lambada_standard piqa sciq wikitext
-
-
-#python ./deepy.py evaluate.py configs/125M.yml owt2_setup_tempnet.yml --eval_results_prefix tempnet \
-#       	--eval_tasks logiqa mathqa hellaswag swag lambada_openai lambada_standard piqa sciq wikitext
-
