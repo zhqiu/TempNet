a8b4ee716ff0849377c1246083cf6fabaf2c85a1
zhqiu <38790914+zhqiu@users.noreply.github.com>
2024-04-06 11:11:33 +0800

    Update run_isogclr_tempnet.sh

--- Bimodal_CL/run_isogclr_tempnet.sh
+++ Bimodal_CL/run_isogclr_tempnet.sh
@@ -1,16 +1,11 @@
 TRANSFORMERS_OFFLINE=1
-
-
 data_path=/data1/VLP             
 imagenet_val_path=/data1/imagenet/val
-
-
-
 train_image_root=cc3m
 data=cc3m
 train_file=clip_train/${data}_train_new.json
 
-lr=2e-4
+lr=8e-4
 
 # sogclr
 """
@@ -34,33 +29,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_
     --ita_type sogclr \
     --sogclr_gamma ${gamma} > sogclr_${data}_gamma${gamma}_${desc}.log & 
 """
-"""
-data=imagenet100
-train_image_root=/data1/imagenet100_labels
 
-# isogclr
-frac=1.0
-desc=isogclr_imagenet100
-gamma=0.8
-rho=4.5
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port=4800 \
-    --use_env clip.py \
-    --data_path ${data_path} \
-    --data ${data} \
-    --train_image_root ${train_image_root} \
-    --output_dir output/isogclr_${data}_gamma${gamma}_rho${rho}_${desc} \
-    --init_model \
-    --use_amp \
-    --epochs 30 --lr 8e-4 \
-    --rho ${rho} \
-    --train_frac ${frac} \
-    --zs_dataset imagenet \
-    --zs_datafolder ${imagenet_val_path} \
-    --ita_type isogclr \
-    --store_tau \
-    --sogclr_gamma ${gamma} > isogclr_${data}_gamma${gamma}_rho${rho}_${desc}.log &
-"""
 
 # isogclr_tempnet
 frac=1.0
